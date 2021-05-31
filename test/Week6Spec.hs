a2af13bb14ad0c59f815c4593047da3384c8c250
Daniela Moura <54228026+merrymeerkat@users.noreply.github.com>
2021-05-31 13:06:25 +0800

    Update Week6Spec.hs

--- test/Week6Spec.hs
+++ test/Week6Spec.hs
@@ -1,16 +1,18 @@
 module Week6Spec (spec) where
 
-import Week6.HW6
-import Test.Hspec
+  import Week6.HW6
+  import Test.Hspec
 
-spec :: Spec
-spec =
-    describe "Week 6" $ do
-        describe "fib" $ do
-            it "should compute the nth fibonacci number (slowly)" $
-                fib 5 `shouldBe` 5
-        describe "fmap" $ do
-            it "should map a function to all the elements in a stream" $
-                fmap (+2) (sRepeat 0) `shouldBe` "[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, ..."
-        
+  spec :: Spec
+  spec =
+      describe "Week 6" $ do
+          describe "fib" $ do
+              it "should compute the nth fibonacci number (slowly)" $
+                  fib 5 `shouldBe` 5
+          describe "minMax" $ do
+              it "should return the minimum and maximum elements of a stream" $
+                  minMax [0,1,2,3456789] `shouldBe` Just (0, 3456789)
+          describe "fastFib" $ do
+              it "should compute the nth fibonacci number (quickly)" $
+                  fastFib 100 `shouldBe` 354224848179261915075
         
